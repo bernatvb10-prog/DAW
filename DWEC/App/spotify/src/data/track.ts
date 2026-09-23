fb2ec510dde4622a5a13ff64bfe7a942312539c3
BernatVilaBoada <bvila683@boscdelacoma.cat>
2026-09-23 20:25:41 +0200

    Vite veure titol primera canço

---
+++ DWEC/App/spotify/src/data/track.ts
@@ -0,0 +1,28 @@
+import type { Track } from "../interface/track";
+
+export const tracks: Track[] = [
+    {
+        id: "2B-CA",
+        title: "Rattle and Hum",
+        artist: "U2",
+        duration: 30
+    },
+    {
+        id: "2B-CX",
+        title: "Rattle and Hum",
+        artist: "U2",
+        duration: 65
+    },
+    {
+        id: "3B-TX",
+        title: "Chicago",
+        artist: "Michael Jackson",
+        duration: 190
+    },
+    {
+        id: "3B-TXY",
+        title: "Chicago",
+        artist: "Michael Jackson",
+        duration: 180
+    },
+]
