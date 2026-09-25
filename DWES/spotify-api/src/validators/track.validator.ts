b6b24926ea9b0a7d4f4fe1916d7a2476d866626e
BernatVilaBoada <bvila683@boscdelacoma.cat>
2026-09-25 17:38:21 +0200

    Afegides longituds de titol i artista

--- DWES/spotify-api/src/validators/track.validator.ts
+++ DWES/spotify-api/src/validators/track.validator.ts
@@ -1,4 +1,5 @@
 import { Track } from "../interfaces/track/track";
+import { MAXARTIST, MAXTITOL } from "../interfaces/track/track.constants";
 
 export function isValidTrack(track: Track): boolean {
 
@@ -6,11 +7,12 @@ export function isValidTrack(track: Track): boolean {
         return false
     }
 
-    if (track.artist.trim().length === 0 ||
-        track.title.trim().length === 0 ||
-        track.duration <= 0)
-    {
-        return false;
-    }
+    const longTitol: number = track.title.trim().replace(/\s+/g, " ").length;
+    const longArtist: number = track.artist.trim().replace(/\s+/g, " ").length;
+
+    if (longArtist === 0 || longArtist > MAXARTIST) { return false; }
+    if (longTitol === 0 || longTitol > MAXTITOL) { return false; }
+    if (track.duration<1) { return false; }
+        
     return true;
 }
